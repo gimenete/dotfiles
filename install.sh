c8b28753ec58814658add58ba639fba4c95c0518
Alberto Gimeno <gimenete@github.com>
2021-09-21 13:12:32 +0200

    Rebranch and aliases

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 BASEDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo "export PATH=\$PATH:$BASEDIR/git-scripts" >> ~/.zshrc
+
+git config --global alias.co 'checkout'
+git config --global alias.st 'status'
+git config --global alias.undo 'reset HEAD^'
+git config --global alias.unstage 'reset HEAD --'
